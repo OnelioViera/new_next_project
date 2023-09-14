0d24a167154107f58aab2ff801283d579695ee98
Onelio Viera <ojvwebdesign@gmail.com>
2023-09-14 13:26:42 -0400

    update files

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,21 @@
-export default function Home() {
+import Jumbotron from "./components/Jumbotron"
+import PlaceHolder from "./components/PlaceHolder"
+
+const HomePage = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="container mx-auto px-4">
-        <h1 className="font-sans text-5xl font-bold p-5">Hello World!</h1>
+      <Jumbotron />
+        <h2 className="font-sans text-4xl font-bold mb-5 mt-8">Home</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam eveniet incidunt fuga, nam minus voluptate optio beatae impedit doloribus officiis sapiente accusamus sunt maxime corporis possimus? Odio aliquam aut quaerat veritatis quidem dolorem aperiam assumenda omnis ipsa voluptates. A, suscipit!
         </p>
+<PlaceHolder />
+<PlaceHolder />
+        
       </div>
         
     </main>
   )
 }
+
+export default HomePage
---
+++ src/app/services/page.jsx
@@ -0,0 +1,17 @@
+export const metadata = {
+  title: 'Services',
+}
+
+const ServicesPage = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="container mx-auto px-4">
+        <h1 className="font-sans text-5xl font-bold my-5">Services</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam eveniet incidunt fuga, nam minus voluptate optio beatae impedit doloribus officiis sapiente accusamus sunt maxime corporis possimus? Odio aliquam aut quaerat veritatis quidem dolorem aperiam assumenda omnis ipsa voluptates. A, suscipit!
+        </p>
+      </div>
+    </main>
+  )
+}
+
+export default ServicesPage
