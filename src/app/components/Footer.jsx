8fda5fb1928c29cf96c633b4f1e65fc221c5b0e3
Onelio Viera <ojvwebdesign@gmail.com>
2023-09-15 13:18:14 -0400

    update files

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <>
       <div className="bg-gray-100 py-4 sticky bottom-0 border-t border-gray-200 px-6 flex items-center justify-between text-gray-600">
       <div className="container">
-      <p className="flex items-start justify-center">Copyright @ 2023 <a href="https://github.com/OnelioViera" className="hover:underline ml-1" target="blank"> Onelio Viera</a></p>
+      <p className="flex items-start justify-center">Copyright @ 2023 <a href="https://github.com/OnelioViera" className="hover:underline ml-1" target="blank"> Onelio Viera. All Rights Reserved.</a></p>
       </div>
     </div>
     </>
